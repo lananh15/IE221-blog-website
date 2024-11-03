368c70b31167f953a332da10cddf1ac9fa2f5e45
lananh15 <lananhngo685@gmail.com>
2024-11-03 16:59:28 +0700

    update database

--- blog_db.sql
+++ blog_db.sql
@@ -1,173 +1,79 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 07:34 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `blog_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
+-- Cấu trúc bảng cho bảng `admin`
 CREATE TABLE `admin` (
-  `id` int(100) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `admin`
---
-
+-- Đang đổ dữ liệu cho bảng `admin`
 INSERT INTO `admin` (`id`, `name`, `password`) VALUES
 (1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2');
 
--- --------------------------------------------------------
+-- Cấu trúc bảng cho bảng `users`
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `comments`
---
+-- Đang đổ dữ liệu cho bảng `users`
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'lananh', 'karrywang685@gmail.com', 'c82915a41e9ef693636dc8666de21f203c2d13bb');
 
-CREATE TABLE `comments` (
-  `id` int(100) NOT NULL,
-  `post_id` int(100) NOT NULL,
-  `admin_id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
-  `comment` varchar(1000) NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `admin_id` int(100) NOT NULL,
-  `post_id` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
+-- Cấu trúc bảng cho bảng `posts`
 CREATE TABLE `posts` (
-  `id` int(100) NOT NULL,
-  `admin_id` int(100) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `admin_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` varchar(10000) NOT NULL,
   `category` varchar(50) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Cấu trúc bảng cho bảng `comments`
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `post_id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `comment` varchar(1000) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `users`
---
+-- Cấu trúc bảng cho bảng `likes`
+CREATE TABLE `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- components/connect.php
+++ components/connect.php
@@ -1,16 +1,16 @@
 <?php
-   $db_host = "bmqecusymwinulglrkb8-mysql.services.clever-cloud.com";
-   $db_username = "uxrgdbrv5awov6h8";
-   $db_password = "nqHPla81ZThxFdT45y8C";
-   $db_name = "bmqecusymwinulglrkb8";
+   $db_host = "mysql-dev-blog-lananhngo685-dev-blog.h.aivencloud.com";
+   $db_username = "avnadmin";
+   $db_password = "AVNS_rJlKFXve7NFmsh7WIVB";
+   $db_name = "defaultdb";
+   $db_port = "14981";
 
-   try {
-      $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
+   try {       
+      $conn = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_username, $db_password);       
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   }
-
-   catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
+   }     
+   catch(PDOException $e) {       
+      echo "Connection failed: " . $e->getMessage();    
    }
 
 ?>
